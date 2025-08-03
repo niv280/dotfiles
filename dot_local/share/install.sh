d7d64a802b3a94c072f569adbdd0ae851fd72104
Niv Edelshtein <nive@rno2-container-xterm-032.prd.it.nvidia.com>
2025-08-03 01:20:21 -0700

    install script

---
+++ dot_local/share/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Install essential tools if not present
+
+sudo apt update
+sudo apt install -y \
+  zsh \
+  git \
+  vim \
+  tmux \
+  nvim \
+  starship \
+  build-essential
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
